730c5c7a08abe46dbad97ed039435a1eefa3e7c3
Developer <dev@example.com>
2026-05-22 23:39:54 +0800

    fix: remove unused useState import in Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { HiOutlineHome, HiOutlineChartBar, HiOutlineLogout, HiOutlinePlus } from 'react-icons/hi'
 
